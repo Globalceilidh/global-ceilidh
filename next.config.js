bd1855c049735267f4aff5de6280894522b10550
Globalceilidh <globalceilidh@gmail.com>
2026-04-12 11:20:14 -0400

    Add Clerk and Supabase, update Next.js, add gitignore, fix warnings

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.jsdelivr.net', 'raw.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+    ],
   },
 }
-
 module.exports = nextConfig
